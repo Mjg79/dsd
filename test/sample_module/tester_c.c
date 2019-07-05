d66ba5c3dcbe6c82d8afd3a993481f9eaed13a25
MaGaroo <amoomajid99@gmail.com>
2019-07-05 18:16:50 +0430

    Change style of sample c tester

--- test/sample_module/tester_c.c
+++ test/sample_module/tester_c.c
@@ -4,29 +4,13 @@
 // sample: #include <../../src/c/not.c>
 
 int main(int argc, char *argv) {
-    string file_name = argv[0];
-    strcat(file_name, ".dat")
-    FILE *in_file;
-    in_file = fopen(file_name,"r");
-    file_name = argv[0];
-    strcat(file_name, "_c.txt")
-    FILE *out_file;
-    out_file = fopen(file_name, "w");
-    if (in_file == NULL) {
-        printf(stderr, "Can not open %s", file_name);
-    }
-    if (out_file == NULL) {
-        printf(stderr, "")
-    }
-    if (in_file && out_file) {
-        string line;
-        while(fscanf(in_file, "%[^\n]\n", line)) {
-            // todo use line as an input for module
-            /* sample
-                int input;
-                sscanf(line, "%d", &input);
-                fprintf(out_file, "%d\n", not(input));
-             */
-        }
-    }
+	string line;
+	while(scanf("%[^\n]\n", line)) {
+		// todo use line as an input for module
+		/* sample
+		   int input;
+		   sscanf(line, "%d", &input);
+		   printf("%d\n", not(input));
+		 */
+	}
 }
