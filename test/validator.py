9b74aca62488fa4ba166eb1f0afec67683b9c621
svafaiet <svafaiet@gmail.com>
2019-07-05 15:04:51 +0430

    Test file with sample for formating added

---
+++ test/validator.py
@@ -0,0 +1,29 @@
+import sys
+import os
+
+if __name__ == "__main__":
+    test_fail_flag = False
+    for folder in sys.argv:
+        output_c_path = os.join(folder, "output_c.txt")
+        output_v_path = os.join(folder, "output_v.txt")
+        if not os.path.exists(output_c_path) or not os.path.exists(output_v_path):
+            print("{} does not match a test_file\n".format(folder))
+        output_c = None
+        output_v = None
+        with open(file=output_c_path, mode="r") as co:
+            with open(file=output_v_path, mode="r") as vo:
+                output_c = co.readlines()
+                output_v = vo.readlines()
+        if len(output_c) != len(output_v):
+            print("{} test failed".format(folder))
+            test_fail_flag = True
+            continue 
+        for i in range(len(output_c)):
+            if output_c[i] != output_v[i]:
+                print("{} test failed".format(folder))
+            test_fail_flag = True
+            continue 
+    if test_fail_flag:
+        print("Test Failed!")
+    else:
+        print("All tests have been passed successfully!")
